bc08ea6937a232946cdbd9066aaa2dd5e4e43f6a
shane <shane86wilkins@gmail.com>
2021-01-30 10:34:31 -0500

    Another one...

--- ElevenNote.Models/NoteCreate.cs
+++ ElevenNote.Models/NoteCreate.cs
@@ -12,7 +12,7 @@ namespace ElevenNote.Models
 
         [Required]
         [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
-        [MaxLength(2, ErrorMessage = "There are too many characters in this field.")]
+        [MaxLength(50, ErrorMessage = "There are too many characters in this field.")]
         public string Title { get; set; }
         [MaxLength(8000)]
         public string Content { get; set; }
